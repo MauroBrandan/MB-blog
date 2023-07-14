5d44e09fe1986d46392ffadfddf55f8c538d65ec
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-07-14 16:25:25 -0300

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,30 @@
-import Link from "next/link"
+import Link from 'next/link'
 
 export function Navbar() {
-  return (
-    <nav className="flex justify-between p-3 mb-3 border-b-2">
-      <div>
-        <Link href={'/'}>MB</Link>
-      </div>
-      <div>
-        <ul className="flex gap-3">
-          <li>
-            <Link href={'/posts'}>Posts</Link>
-          </li>
-          <li>
-            <a href="https://maurobrandan.com/" target="_blank" rel="noopener noreerrer">Sobre mi</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
+	return (
+		<nav className='flex justify-between items-center py-5 text-lg leading-5'>
+			<div className='text-3xl font-bold'>
+				<Link href={'/'}>MB</Link>
+			</div>
+			<div className='"py-1 font-medium sm:py-4'>
+				<ul className='flex gap-8'>
+					<li>
+						<Link className='hover:underline' href={'/posts'}>
+							Posts
+						</Link>
+					</li>
+					<li>
+						<a
+							href='https://maurobrandan.com/'
+							target='_blank'
+							rel='noopener noreerrer'
+							className='hover:underline'
+						>
+							Sobre mí
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	)
 }
