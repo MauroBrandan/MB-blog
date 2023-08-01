8068fcb719fc2a724d2e2260533d7b4bd0ada59e
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-08-01 00:05:55 -0300

    Add icons to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,25 @@
+import { CodeBracketsIcon, GitHubIcon, LinkedinIcon } from '@/components/Icons'
+
 export function Footer() {
-  return (
-    <footer className='flex justify-center items-center gap-5 font-bold italic text-lg mt-5 w-full h-32'>
-		<a href="http://maurobrandan.com" target="_blank" rel="noopener noreferrer">maurobrandan.com</a>
-		<a href="https://github.com/MauroBrandan" target="_blank" rel="noopener noreferrer">GitHub</a>
-		<a href="https://www.linkedin.com/in/maurobrandan" target="_blank" rel="noopener noreferrer">Linkedin</a>
-	</footer>
-  )
+	return (
+		<footer className='flex justify-center items-center gap-5 font-bold italic text-lg mt-5 w-full h-32'>
+			<a href='http://maurobrandan.com' target='_blank' rel='noopener noreferrer'>
+				<CodeBracketsIcon />
+			</a>
+			<a
+				href='https://github.com/MauroBrandan'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<GitHubIcon />
+			</a>
+			<a
+				href='https://www.linkedin.com/in/maurobrandan'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<LinkedinIcon />
+			</a>
+		</footer>
+	)
 }
